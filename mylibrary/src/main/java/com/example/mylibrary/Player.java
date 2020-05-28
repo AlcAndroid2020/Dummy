d7c224e88a1ed8a415555848b4e348f3ed9974ea
cstanALC <cstan.alc@gmail.com>
2020-05-28 14:45:27 +0800

    Activate Library

---
+++ mylibrary/src/main/java/com/example/mylibrary/Player.java
@@ -0,0 +1,31 @@
+package com.example.mylibrary;
+
+public class Player {
+    private String Name;
+    private int age;
+    private String Location;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+}
